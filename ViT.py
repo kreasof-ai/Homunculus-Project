c0c7292f5990a771c848ac33d6159d8d177514c6
akbar2habibullah <akbar2habibullah@gmail.com>
2024-07-24 05:42:02 +0700

    update

--- ViT.py
+++ ViT.py
@@ -2,7 +2,6 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-from einops import rearrange
 from RoPE import RotaryPositionalEmbedding2D, apply_rotary_pos_emb_2d
 from main import TransformerBlock
 
